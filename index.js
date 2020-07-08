57f5324579c1a1afc0f1206463623c1b6a9bbf1a
Daniel <danielfealmeida@gmail.com>
2020-07-08 17:04:11 -0300

    changed CORS Police

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ import cors from "cors"
 const app = express()
 const database = createDatabase();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+})
+
 app.listen(3000, () => {
     console.log("[SERVER] => Server listening.")
     database.start()
