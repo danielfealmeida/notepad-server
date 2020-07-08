97f40017ee5ce538028524cf50a7c317fca971fe
Daniel <danielfealmeida@gmail.com>
2020-07-08 17:05:58 -0300

    changed CORS Police

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
+    next();
 })
 
 app.listen(3000, () => {
