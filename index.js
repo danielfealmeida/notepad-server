343d38e12368bc1793ec0d52fe53f8bf1c1672a6
Daniel <danielfealmeida@gmail.com>
2020-09-01 12:04:36 -0300

    Changed Port

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("[SERVER] => Server listening.")
     database.start()
 })
